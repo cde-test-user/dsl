0e28bc89df5fc022f3b45d289fa5e3665cc4a610
CDD Test User <cde.test.user@gmail.com>
2018-11-11 11:55:25 +0200

    Create todo-spec2.js

---
+++ todo-spec2.js
@@ -0,0 +1,8 @@
+describe('CA MSF Raffle UI Test - cdbu.io Environment', function() {
+  it('should add a todo', function() {
+    browser.ignoreSynchronization = true;
+    browser.get('http://preprod.cdbu.io');
+    element(by.css('[name="name"]')).sendKeys('Selenium Test User2');
+    element(by.buttonText('I\'m In!')).click();
+    });
+});
